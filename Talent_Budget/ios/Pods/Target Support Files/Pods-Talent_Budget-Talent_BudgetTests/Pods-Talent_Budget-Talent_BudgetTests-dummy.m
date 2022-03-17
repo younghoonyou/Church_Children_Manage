ae1a06b8d43ca311ac987555431e3f014380bc1e
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-03-17 16:52:44 +0900

    Deploy

--- Talent_Budget/ios/Pods/Target Support Files/Pods-Talent_Budget-Talent_BudgetTests/Pods-Talent_Budget-Talent_BudgetTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Talent_Budget_Talent_BudgetTests : NSObject
-@end
-@implementation PodsDummy_Pods_Talent_Budget_Talent_BudgetTests
-@end
--- Talent_Budget/ios/Pods/Target Support Files/Pods-Talent_Budget/Pods-Talent_Budget-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Talent_Budget : NSObject
-@end
-@implementation PodsDummy_Pods_Talent_Budget
-@end
