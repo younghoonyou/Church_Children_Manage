6bd2ee8042a2a33c5f4612873a2273f1376a2c00
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-03-17 16:44:25 +0900

    git

---
+++ Talent_Budget/ios/Pods/Target Support Files/Pods-Talent_Budget-Talent_BudgetTests/Pods-Talent_Budget-Talent_BudgetTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Talent_Budget_Talent_BudgetTests : NSObject
+@end
+@implementation PodsDummy_Pods_Talent_Budget_Talent_BudgetTests
+@end
---
+++ Talent_Budget/ios/Pods/Target Support Files/Pods-Talent_Budget/Pods-Talent_Budget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Talent_Budget : NSObject
+@end
+@implementation PodsDummy_Pods_Talent_Budget
+@end
